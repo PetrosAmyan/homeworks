c4c301add7e80045b19b9e8409af8e64d7207e5b
Petros <petrosamyan2003@gmail.com>
2025-10-29 20:30:03 +0400

    basic types

---
+++ work_withdouble.cpp
@@ -0,0 +1,149 @@
+#include <iostream>
+
+//const int sumof(int x, int y) {
+	//	return x + y;
+//}
+
+// struct Point {
+// 		double x;
+// 		double y;
+// };
+
+// enum DAYS {
+// 	MON,
+// 	THUS,
+// 	WED,
+// } ;
+// void foo(int arr[][2])
+// {
+// 	std::cout << arr[0][1] << "\n";
+// }
+
+//void foo(int& a)
+//{
+//	a++;
+//}
+
+// static int a = 100;
+// void foo()
+// {
+// 	static int x = 12;
+// //x++;
+// 	x++;
+// 	std::cout << x << "\n";
+	
+// }
+
+int main()
+{
+	//static int b = 12;
+
+	//foo();
+	//foo();
+	//foo();
+	//a = 120;
+	//std::cout << a << "\n";
+	//static int x = 12;
+	//int x = 12;
+	//foo(x);
+	//std::cout << x << "\n";
+
+	// int arr2d[2][2] = {
+	// 	{1, 2},
+	// 	{3, 4}
+	// };
+	// std::cout << arr2d[0][1];
+	// foo(arr2d);
+	// Point p;
+	// DAYS en = MON;
+	// if (en == 0) {
+	// 		std::cout << "monday";
+	// }
+	// int res = sumof(2,3);
+	// res = 12;
+	//double a = 3.12030001;
+	//double b = 4.120600002;
+	//int x = 3;
+//	double y = (int)x;
+	//std::cout << a + b << "\n";
+	//std::cout << y << "\n";
+	//int x = static_cast<int>(a);
+	//std::cout << x << "\n";
+	//float q = 3.12322123;
+	//double y = static_cast<double>(q);
+	//std::cout << y << "\n";
+	//double q = 2.12353364;
+	//float y = static_cast<float>(q);
+//	std::cout << y;
+
+	//double x = 0.1213;
+	//int* ptr = reinterpret_cast<int*>(&x);
+	//std::cout << *ptr << "\n";
+	//double y = 0.12101;
+	//int* ptr2 = reinterpret_cast<int*>(&y);
+//	std::cout << *ptr2 << "\n";
+
+	// double x = 0.00000000000010;
+	// double prev;
+	// while (x > 0) {
+	// 		prev = x;
+	// 		x /= 0.1;
+	// }
+	// std::cout << prev;
+
+
+	//double x = 12.21442;
+	//int* y = reinterpret_cast<int*>(&x);
+	//std::cout << y << "\n";
+	
+//	return -1;
+	// auto i = 12;
+	
+	// const int x = 12;
+	// int z = 10;
+	// constexpr int y = 12*(x);
+	// //constexpr int a = 12*(z);
+	// //y = 20*(x);
+
+	// char x = '1';
+	// switch (x)
+	// {
+	// 	case '1':
+	// 		std::cout << "here";
+	// 		break;
+	// 	case '3':
+	// 		std::cout << "second";
+	// 		break;
+	// 	default:
+	// 	break;
+
+
+	// }
+
+	//int v[] = {2, 3, 12, 42};
+	//int* ptr = arr;
+//	int arr[3] = {1, 2, 3};
+//	int& ptr2 = arr;
+//	std::cout << ptr2 <<  "\n";
+	//int a = 100;
+// 	int x = 12;
+// 	int& r = x;
+// 	r = 20;
+// 	r = a;
+// 	std::cout << r << "\n";
+// 	std::cout << x << "\n";
+
+//std::cout << "\n";
+//int arr[] = {1, 2, 3, 4};
+//int* ptr = arr;
+//ptr[0] = 10;
+//std::cout << arr[0];
+//std::cout << ptr << " " << arr;
+
+
+double x = 0.00001223134;
+int* ptr = reinterpret_cast<int*>(&x);
+std::cout << *ptr  << "\n";
+
+
+}
