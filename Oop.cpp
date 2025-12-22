f554f982fea5b621b5f8f3a6050f8998db5eb311
Petros <petrosamyan2003@gmail.com>
2025-12-22 22:09:27 +0400

    inheritance and overloading

---
+++ Oop.cpp
@@ -0,0 +1,156 @@
+#include <iostream>
+#include <limits>
+#include <vector>
+#include <bits/stdc++.h>
+
+// class Calculator {
+// 	public:
+// 	int add(int a, int b) {
+// 			return a + b;
+// 	}
+
+// 	int add(int a, int b, int c) {
+// 			return a + b + c;
+// 	}
+
+// 	int add(int a, double c) {
+// 			return a + c;
+// 	}
+
+// 	double add(double a, double b) {
+// 			return a + b;
+// 	}
+// };
+
+// class Person {
+// 		private:
+// 		int age;
+// 		public:
+// 		Person(int _age) {
+// 			age = _age;
+// 		}
+
+// 		Person operator+(const Person& other) {
+// 			std::cout << "called operator +\n";	
+// 			return Person(this->age + other.age);
+// 		}
+
+// 		bool operator==(const Person& other) {
+// 				return this->age == other.age;
+// 		}
+
+// 		bool operator!=(const Person& other) {
+// 				return this->age != other.age;
+// 		}
+// };
+
+
+// class Person {
+// 	private:
+// 	int age;
+// 	public:
+// 	Person(int _age) {
+// 		age = _age;
+// 		std::cout << "called based class constructor\n";
+// 	}
+
+// 	int getAge() {
+// 			return age;
+// 	}
+
+// };
+
+
+// class Student : public Person {
+// 	private:
+// 	int course;
+// 	public:
+// 	Student(int _age, int _course) : Person(_age) {
+// 		std::cout << "called derived class constructor\n";
+// 		course = _course;
+// 	}	
+
+// void foo() {
+// 		std::cout << getAge() << "\n";
+// }
+
+// };
+
+// class Teacher : Student {
+// 	public :
+// 	Teacher(int _age, int _course) : Student(_age, _course) {
+// 			std::cout << "teacher constructor\n";
+// 	}
+	
+
+// };
+
+
+// class Base {
+// public:
+//     enum Kind { PERSON, STUDENT, TEACHER };
+//     Kind type;
+
+//     Base(Kind k) : type(k) {}
+// };
+
+// class Student : public Base {
+// public:
+//     Student() : Base(Base::STUDENT) {}
+// };
+
+// class Teacher : public Base {
+// public:
+//     Teacher() : Base(Base::TEACHER) {}
+// };
+
+
+// class Person {
+// 	public:
+// 	virtual void printInfo() = 0;
+// };
+
+// class Student : public Person {
+// 	public:
+// 	void  printInfo() override {
+// 		std::cout << "i am student\n";
+// 	}
+
+// };
+
+// class Teacher : public Student {
+// 	public:
+// 	void printInfo() override {
+// 			std::cout << "i am teacher\n";
+// 	}
+// };
+
+int main()
+{
+	//Person* ob = new Student(); 
+	//ob->printInfo();
+//	Person* obj = new Teacher();
+	//obj->printInfo();
+	// 	Person obj1(3), obj2(3);
+// 	Person obj3 = obj1 + obj2;
+// 	if (obj1 == obj2) {
+// 			std::cout << "yes";
+// 	}
+
+// 	//std::cout << std::numeric_limits<float> :: max() << "\n";
+// 	// Calculator a;
+	
+// 	// std::cout << a.add(1, 2, 3) << "\n";
+// 	// std::cout << a.add(1.3, 2.5) << "\n";
+// 	// std::cout << a.add(1, 2.5) << "\n";
+
+	//Student obj(12, 1);
+	//obj.getAge();
+//	Teacher obj(12, 4);
+
+
+	std::vector<std::pair<char, int>> groups;
+
+
+
+ }
